5da31f4a6742f4b01243e3d10065a9697725ffdd
Phanomphon Yotchon <mosass@users.noreply.github.com>
2017-06-11 00:04:25 +0700

    Add SixpodSocker

--- SixpodRemoteApp/SixpodRemoteApp/MainWindow.xaml.cs
+++ SixpodRemoteApp/SixpodRemoteApp/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SixpodRemoteApp
 {
@@ -20,9 +10,131 @@ namespace SixpodRemoteApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SixpodSocket remoteRobot;
+        private bool walking;
+        private REMOTECMD gaitType;
+
         public MainWindow()
         {
             InitializeComponent();
+            walking = false;
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            remoteRobot = new SixpodSocket(txt_ipaddress.Text, this);
+
+            sld_stepTime.Minimum = 1;
+            sld_stepTime.Maximum = 10;
+            sld_stepTime.SmallChange = 1;
+            sld_stepTime.Value = 5;
+
+            sld_gaitZPercent.Minimum = 0;
+            sld_gaitZPercent.Maximum = 100;
+            sld_gaitZPercent.SmallChange = 1;
+            sld_gaitZPercent.LargeChange = 5;
+            sld_gaitZPercent.Value = 50;
+
+            gaitType = REMOTECMD.WALKING_WAV;
+            rdo_wave.IsChecked = true;
+        }
+
+        private void btn_connect_Click(object sender, RoutedEventArgs e)
+        {
+            remoteRobot.setIpAddress(txt_ipaddress.Text);
+
+            if (remoteRobot.logs_recv_flag)
+            {
+                remoteRobot.logs_recv_flag = false;
+                remoteRobot.StopClient();
+            }
+            else
+            {
+                new Task(() => {
+                    remoteRobot.StartClient();
+                    remoteRobot.StartDebugClient();
+                }).Start();
+
+            }
+        }
+
+        private void txt_log_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txt_log.ScrollToEnd();
+        }
+
+        private void sld_stepTime_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txt_set_stepTime.Text = (sld_stepTime.Value / 10f).ToString("F2");  
+        }
+
+        private void sld_gaitZPercent_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txt_set_gaitZPercent.Text = ((int) sld_gaitZPercent.Value).ToString("G") + " %";
+        }
+
+        private void btn_apply_Click(object sender, RoutedEventArgs e)
+        {
+            // if connected
+            if (remoteRobot.logs_recv_flag)
+            {
+                float step_time_val = (float) (sld_stepTime.Value / 10f);
+                float stepZUp_val = (float) (sld_gaitZPercent.Value / 100f);
+                remoteRobot.sendCmd(REMOTECMD.SET_STEP_TIME, step_time_val);
+                remoteRobot.sendCmd(REMOTECMD.SET_STEP_UPZ, stepZUp_val);
+            }
+        }
+
+        private void btn_walking_Click(object sender, RoutedEventArgs e)
+        {
+            if(remoteRobot.logs_recv_flag)
+            {
+                if (walking)
+                {
+                    remoteRobot.sendCmd(REMOTECMD.WALKING_STOP);
+                    walking = false;
+                    btn_walking.Content = "Start";
+                }
+                else
+                {
+                    remoteRobot.sendCmd(gaitType);
+                    walking = true;
+                    btn_walking.Content = "Stop";
+                }
+            }
+        }
+
+        private void rdo_wave_Checked(object sender, RoutedEventArgs e)
+        {
+            gaitType = REMOTECMD.WALKING_WAV;
+        }
+
+        private void rdo_ripple_Checked(object sender, RoutedEventArgs e)
+        {
+            gaitType = REMOTECMD.WALKING_RIPP;
+        }
+
+        private void rdo_tripod_Checked(object sender, RoutedEventArgs e)
+        {
+            gaitType = REMOTECMD.WALKING_TRI;
+        }
+
+        private void btn_updatefilelist_Click(object sender, RoutedEventArgs e)
+        {
+            string[] filename = remoteRobot.sendCmd(REMOTECMD.GET_FILE_LIST).Split(',');
+            lsb_filename.Items.Clear();
+            foreach (string s in filename)
+            {
+                lsb_filename.Items.Add(s);
+            }
+        }
+
+        private void lsb_filename_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(lsb_filename.SelectedIndex >= 0)
+            {
+                remoteRobot.sendCmd(REMOTECMD.SET_SELECT_POS, lsb_filename.SelectedIndex + 1);
+            }
         }
     }
 }
---
+++ SixpodRemoteApp/SixpodRemoteApp/SixpodSocket.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Threading;
+
+namespace SixpodRemoteApp
+{
+    enum REMOTECMD
+    {
+        SET_STEP_TIME = 31,
+        SET_STEP_UPZ = 32,
+        SET_SELECT_POS = 33,
+        GET_FILE_LIST = 34,
+        WALKING_STOP = 40,
+        WALKING_WAV = 41,
+        WALKING_RIPP = 42,
+        WALKING_TRI = 43
+    }
+
+    class SixpodSocket
+    {
+        private MainWindow win;
+        private IPAddress remoteIpAddress;
+
+        private Socket remoteSocket;
+
+        private const int remotePort = 9000;
+        private const int debugPort = 9001;
+
+        public bool logs_recv_flag { get; set; }
+
+        public SixpodSocket(String Ip, MainWindow Window)
+        {
+            win = Window;
+            remoteIpAddress = IPAddress.Parse(Ip);
+
+            logs_recv_flag = false;
+        }
+
+        public void setIpAddress(string Ip)
+        {
+            remoteIpAddress = IPAddress.Parse(Ip);
+        }
+
+        public void setIpAddress(IPAddress Ip)
+        {
+            remoteIpAddress = Ip;
+        }
+
+        private void logsLine(String msg, bool newLine = true)
+        {
+            if (newLine)
+            {
+                win.txt_log.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                new Action(() => win.txt_log.AppendText(msg + "\r\n")));
+            }
+            else
+            {
+                win.txt_log.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                new Action(() => win.txt_log.AppendText(msg)));
+            }
+        }
+
+        private void setUIOnlineStatus(bool onlineStatus)
+        {
+            string lbl_status, btn_status;
+            if (onlineStatus)
+            {
+                lbl_status = "Online";
+                btn_status = "Disconnect";
+                logs_recv_flag = true;
+            }
+            else
+            {
+                lbl_status = "Offline";
+                btn_status = "Connect";
+                logs_recv_flag = false;
+            }
+
+            win.txt_statusmsg.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                new Action(() => win.txt_statusmsg.Content = lbl_status));
+
+            win.btn_connect.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                new Action(() => win.btn_connect.Content = btn_status));
+        }
+
+        public void StartClient()
+        {
+            // Connect to a remote device.  
+            try
+            {
+                // Create a TCP/IP  socket.  
+                remoteSocket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+                IPEndPoint remoteEP = new IPEndPoint(remoteIpAddress, remotePort);
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                try
+                {
+                    remoteSocket.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        remoteSocket.RemoteEndPoint.ToString());
+
+                    logsLine("Socket connected to " + remoteSocket.RemoteEndPoint.ToString());
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                    logsLine("ArgumentNullException : " + ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                    logsLine("SocketException : " + se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                    logsLine("Unexpected exception : " + e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                logsLine(e.ToString());
+            }
+        }
+
+        public void StopClient()
+        {
+            // Connect the socket to the remote endpoint. Catch any errors.  
+            try
+            {
+                // Release the socket.  
+                remoteSocket.Shutdown(SocketShutdown.Both);
+                remoteSocket.Close();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                logsLine("ArgumentNullException : " + ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+                logsLine("SocketException : " + se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                logsLine("Unexpected exception : " + e.ToString());
+            }
+        }
+
+        public string sendCmd(REMOTECMD cmd, float val = 0)
+        {
+            String sb;
+            switch (cmd)
+            {
+                case REMOTECMD.SET_STEP_TIME:
+                case REMOTECMD.SET_STEP_UPZ:
+                    sb = String.Format("{0:D} {1:F}", cmd, val);
+                    writeToSocket(sb);
+                    return readFormSocket();
+                case REMOTECMD.SET_SELECT_POS:
+                    sb = String.Format("{0:D} {1:D}", cmd, (int) val);
+                    writeToSocket(sb);
+                    return readFormSocket();
+                case REMOTECMD.GET_FILE_LIST:
+                case REMOTECMD.WALKING_RIPP:
+                case REMOTECMD.WALKING_TRI:
+                case REMOTECMD.WALKING_WAV:
+                case REMOTECMD.WALKING_STOP:
+                    sb = String.Format("{0:D}", cmd);
+                    writeToSocket(sb);
+                    return readFormSocket();
+                default:
+                    return "NACK";
+            }
+        }
+
+        private void writeToSocket(string buf)
+        {
+            // Connect the socket to the remote endpoint. Catch any errors.  
+            try
+            {
+                // Send the data through the socket.  
+                int bytesSent = remoteSocket.Send(Encoding.ASCII.GetBytes(buf));
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                logsLine("ArgumentNullException : " + ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+                logsLine("SocketException : " + se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                logsLine("Unexpected exception : " + e.ToString());
+            }
+        }
+
+        private string readFormSocket()
+        {
+            // Connect the socket to the remote endpoint. Catch any errors.  
+            byte[] bytes = new byte[2048];
+            try
+            {
+                // Send the data through the socket.  
+                int bytesRec = remoteSocket.Receive(bytes);
+                return Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            }
+            catch (ArgumentNullException ane)
+            {
+                return String.Format("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                return String.Format("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                return String.Format("Unexpected exception : {0}", e.ToString());
+            }
+        }
+
+        public void StartDebugClient()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new byte[1024];
+
+            // Connect to a remote device.  
+            try
+            {
+                // Create a TCP/IP  socket.  
+                Socket sender = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+                IPEndPoint remoteEP = new IPEndPoint(remoteIpAddress, debugPort);
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    logsLine("Socket connected to " + sender.RemoteEndPoint.ToString());
+
+                    setUIOnlineStatus(true);
+
+                    // Encode the data string into a byte array.  
+                    //byte[] msg = Encoding.ASCII.GetBytes("GetLogs" + Environment.NewLine);
+
+                    // Send the data through the socket.  
+                    //int bytesSent = sender.Send(msg);
+
+                    while (logs_recv_flag)
+                    {
+                        // Receive the response from the remote device.
+
+                        int bytesRec = sender.Receive(bytes);
+                        Console.WriteLine("Echoed test = {0}",
+                                    Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                        logsLine(Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                    }
+
+                    // Release the socket.  
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                    setUIOnlineStatus(false);
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                    logsLine("ArgumentNullException : " + ane.ToString());
+                    setUIOnlineStatus(false);
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                    logsLine("SocketException : " + se.ToString());
+                    setUIOnlineStatus(false);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                    logsLine("Unexpected exception : " + e.ToString());
+                    setUIOnlineStatus(false);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                logsLine(e.ToString());
+                setUIOnlineStatus(false);
+            }
+        }
+    }
+}
